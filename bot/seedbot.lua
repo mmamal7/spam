d5a41f3f7a13a94a663926477338ea141b8cb96d
mmamal7 <shahi.m.0930@gmail.com>
2016-06-01 00:02:16 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "get",
     "broadcast",
     "download_media",
-    "invite",
+    "sudo_users = {207118312},--Sudo users
     "all",
     "leave_ban",
     "admin"
